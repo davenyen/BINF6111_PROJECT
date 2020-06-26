3386d5ff6e9f37f97b54d794b3517179adb1503a
student <student@rna.ad.unsw.edu.au>
2020-06-26 13:51:03 +1000

    testing commit from vs terminal

--- cell_assign.py
+++ cell_assign.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv
 
 # Read in matrix csv
 # - Associate barcode from read one to sequence in read two
@@ -7,3 +8,35 @@ import sys
 # created
 # - Identify cell barcode from read one, allocate sequence from read 2 to a
 # particular group 
+
+###### TO DO ######
+# read pilot crop
+# take x y position (use instead of barcode)
+# grab read 2 sequence and header
+# write as fastq files
+# group separated by (target (group))
+# look at barcode from read 1 and search matrix (find target)
+
+# Make dictionary for O(1)
+# quality scores?
+
+# FUNCTIONS
+
+# Reads matrix csv and saves as a data structure (dictionary)
+def read_matrix (csv_matrix):
+	with open(csv_matrix, 'r') as file:
+	    reader = csv.reader(file)
+	    i = 0
+	    for row in reader:
+	        print(row)
+	        i += 1
+	        if i == 5:
+	        	break
+	pass
+
+# MAIN
+if __name__ == '__main__':
+
+	csv_matrix = sys.argv[1]
+	read_matrix (csv_matrix)
+	
