c74d70fa413d201b5e8ee6478d2a2267d7e5b45e
student <student@rna.ad.unsw.edu.au>
2020-06-27 23:57:03 +1000

    Fixed, saves 4 lines per pertubation now.

--- cell_assign.py
+++ cell_assign.py
@@ -1,98 +1,41 @@
 import sys
-import collections
 import os
-import time
-import csv
-from itertools import islice
-from functions import *
+from functions import read_matrix, create_target_directory, create_sorted_fastq_file, coordinates_barcodes_dictionary, error_check
 
+# REMOVE COUNTS FROM FUNCTIONS.PY FOR FULL OUTPUT (ATM LIMITED TO 10,000)
+
+# Input: python3 cell_assign.py {matrix.csv} {read1.fastq} {read2.fastq}
+# Output: sorted_target_groups/{lots of groups}/group.fastq (for each group)
+# Sample run cmd line:
 # python3 cell_assign.py /Users/student/BINF6111_2020/data/test_barcode.csv /Users/student/BINF6111_2020/test/output/PilotCROP_C_1_S1_L001_R1_001.fastq /Users/student/BINF6111_2020/test/output/PilotCROP_C_1_S1_L001_R2_001.fastq
 
-# to avoid a stupid error i have to do
-__all__ = ["read_matrix", "create_target_directory", "coordinates_barcodes_dictionary", "consume"]
-
-# cmd line -> python3 {matrix} {read1} {read2}
-
 # Read in matrix csv
-# - Associate barcode from read one to sequence in read two
+# - Associate barcode from read one to sequence in read 2
 # - When file is list of groups, a list of cell barcodes from those groups are
-# created
+# 	created
 # - Identify cell barcode from read one, allocate sequence from read 2 to a
-# particular group 
-
-###### TO DO ######
-# 1. match coordinate of read1 with read2
-# 2. match read1 barcode with matrix (gets target)
-# 3. grab read2 sequence and put into group directory (target) (WILL JUST GRAB HEADER AND SEQUENCE FOR NOW)
-#     - quality scores as well and header?
-# 4. error cases and handling last!
-
-############################################################################
-################################ FUNCTIONS #################################
-############################################################################
-
-# Writes to sorted fastq files 
-def write_fastq (void):
-	pass
+# 	particular group 
 
 # MAIN
 if __name__ == '__main__':
 
+	# BLANK SLATE, removes the folder so you can create new sorted files
+	if os.path.isdir("sorted_target_groups") == True:
+		os.system("rm -r sorted_target_groups")
+	
 	# VARIABLES
 	csv_matrix = sys.argv[1]
 	read_one = sys.argv[2]
 	read_two = sys.argv[3]
-
 	
+	# Run error checking
+	error_check (csv_matrix, read_one, read_two)
+	
+	# Main functions
 	barcode_table = read_matrix (csv_matrix)
 	create_target_directory (barcode_table)
 	coordinates_barcodes = coordinates_barcodes_dictionary (read_one)
+	create_sorted_fastq_file (read_two, barcode_table, coordinates_barcodes)
 
-	# Matches coordinates read1 - read2
-	file = open(read_two)
-	coordinates = ''
-	sequence = ''
-	success_count = 0
-	count = 0
-	header = ''
-
-	with file:
-		for i, line in enumerate(file, 1):
-		
-			# check line if it is header, save to maybe print later
-			if line[0] == "@":
-				header = line
-				coordinates = ':'.join(line.split(':')[4:6])
-
-			# if it's not a header must be a sequence
-			else:
-				sequence = ''.join(line[0:])
-
-				# GET TARGET/GROUP then write a fastq file with read2 data into group/directory
-				if coordinates in coordinates_barcodes:
-					if coordinates_barcodes[coordinates] in barcode_table.keys():
-						try:
-							target_file = ("sorted_target_groups/{}/{}.fastq".format(barcode_table[coordinates_barcodes[coordinates]], barcode_table[coordinates_barcodes[coordinates]]))
-							f = open(target_file, "w")
-							f.write("{}\n{}".format(header, sequence))
-							f.close()
-						except:
-							target_file = ("sorted_target_groups/{}/{}.fastq".format(barcode_table[coordinates_barcodes[coordinates]], barcode_table[coordinates_barcodes[coordinates]]))
-							f = open(target_file, "a")
-							f.write("{}\n{}".format(header, sequence))
-							f.close()
-
-						success_count += 1
-
-			count += 1
-			if count == 100:
-				break
-
-			# will skip lines 3 and 4 for performance
-			if not i % 2:
-				consume(file, 2)
-
-	print("\nSUCCESS COUNT = {}".format(str(success_count)))
-
-
+	print("\nCELL ASSIGNMENT SUCCESSFUL\n")
 
