9f6c3c11de9941311e079c3b7c1e75eb46229b7b
student <student@rna.ad.unsw.edu.au>
2020-07-07 01:54:54 +1000

    UPdate with master

--- cell_assign.py
+++ cell_assign.py
@@ -1,3 +1,6 @@
+# Author: David Nguyen
+# Function: Assigns cells to groups/targets
+
 import sys
 import os
 import time
@@ -15,21 +18,21 @@ from functions import read_matrix, create_target_directory, create_sorted_fastq_
 # (REFACTOR [1]) TIME TAKEN TO RUN FULL OUTPUT ON test_r1 AND test_r2 = 0.8 minutes [avg of 5 runs]
 # (REFACTOR [2]) TIME TAKEN TO RUN FULL OUTPUT ON test_r1 AND test_r2 = 1.33 minutes [avg of 5 runs]
 
-# OPTIMISATION IDEAS:
+# OPTIMISATION IDEAS: [DO 5 FOR MAXIMUM SPEED FOR SURE]
 # (1) Assume some barcodes are not in the matrix, then do [if barcode not in matrix: skip] (speeds up read1)
 # (2) Assume coordinates do not always match, then do [if coordinates not in read1: skip] (speeds up read2)
 # (3) Assume coordinates always match, then do [if read1[coordinate] barcode not in matrix: skip] (speeds up read2)
 # (4) Assume parse_fastq processes read1, get variables from that function simultaneously for speed 
+# (5) Open all fastq files for appending, and then close all of them at the end (should speed up ALOT because opening files and closing takes a lot of time)
 
 # Input: python3 cell_assign.py {matrix.csv} {read1.fastq} {read2.fastq}
 # Output: sorted_target_groups/{lots of groups}/group.fastq (for each group)
+
 # Sample run cmd line:
 # python3 cell_assign.py /Users/student/BINF6111_2020/data/test_barcode.csv /Users/student/BINF6111_2020/test/output/PilotCROP_C_1_S1_L001_R1_001.fastq /Users/student/BINF6111_2020/test/output/PilotCROP_C_1_S1_L001_R2_001.fastq
 
-
 # TEST RUN USE THIS:
 # python3 cell_assign.py /Users/student/BINF6111_2020/data/test_barcode.csv /Users/student/BINF6111_2020/test/output/test_L001_R1_001.fastq /Users/student/BINF6111_2020/test/output/test_L001_R2_001.fastq 
-
 
 # Read in matrix csv
 # - Associate barcode from read one to sequence in read 2
@@ -41,6 +44,7 @@ from functions import read_matrix, create_target_directory, create_sorted_fastq_
 # TO DO
 # (1) Add into master_script 
 # (2) Toggle num_lines to read files length
+# (3) Optimise runtime 
 
 # MAIN
 if __name__ == '__main__':
@@ -61,4 +65,4 @@ if __name__ == '__main__':
 	create_sorted_fastq_file (read_two, barcode_table, coordinates_barcodes, dir_name)
 
 	print("\nCELL ASSIGNMENT SUCCESSFUL")
-	#print("Runtime = {} h/m/s.\n".format(str(datetime.timedelta(seconds=time.time() - start_time))))
+	print("Runtime = {} h/m/s.\n".format(str(datetime.timedelta(seconds=time.time() - start_time))))
