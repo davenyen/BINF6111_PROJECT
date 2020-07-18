88f8089ebd9ca7126152bd7f6b5de1ea48cc669c
student <student@rna.ad.unsw.edu.au>
2020-07-19 03:52:46 +1000

    Added in splitting and threading david

--- parse_lane.py
+++ parse_lane.py
@@ -7,7 +7,8 @@ import re
 import sys
 import time
 import datetime
-from functions import *
+from functions import read_matrix, create_indices_list, create_coordinates_barcodes_dictionary, create_target_directory
+from functions import create_fastq_files, split_read_two, create_sorted_fastq_file, write_to_log, create_threads
 
 ## TODO
 ## Error checking will be done by master script
@@ -22,10 +23,10 @@ if __name__ == '__main__':
 	desired_barcodes = sys.argv[3]
 	indices_path = sys.argv[4]
 	experiment_name = sys.argv[5]
-	num_threads = 0 # sys.argv[6]
-	append_target_directory = True # sys.argv[7]
+	num_threads = 8 # sys.argv[6]
+	append_target_directory = False # sys.argv[7]
+	split_shortcut_dir = False # sys.argv[8] ONLY FOR TESTING PURPOSES IF YOU DONT PUT A DIR HERE IT WILL SPLIT
 	
-
 	## Derived
 	working_dir = os.path.dirname(read_one)
 	read_two = read_one.replace('_R1_','_R2_')
@@ -35,7 +36,7 @@ if __name__ == '__main__':
 	## Settings (can tweak)
 	log_path = working_dir + '/pipeline_log.txt'
 	output_dir = (working_dir + '/SORTED_GROUPS')
-	maaaaaaaaany_lines = 10000000
+	maaaaaaaaany_lines = 100000000
 
 	## Writing to log
 	message.append('read_one = ' + read_one)
@@ -60,16 +61,15 @@ if __name__ == '__main__':
 	create_fastq_files (dir_name, indices_list, group_barcode_matrix)
 
 	start_time = time.time()
-	# Function to split files that that are large
-
 	coord_barcode_matrix, line_count = create_coordinates_barcodes_dictionary (read_one, group_barcode_matrix, desired_barcodes, indices_list)
-
 
 	if line_count >= maaaaaaaaany_lines and num_threads != 0:
 		# split file function, optimise on num_threads variable
 		# run on split files
-		message.append("LARGE FILES NOT YET IMPLEMENTED")
-		pass
+		split_files, tmp_dir = split_read_two (read_two, line_count, 8, split_shortcut_dir)
+		create_threads (split_files, group_barcode_matrix, coord_barcode_matrix, dir_name, indices_list)
+		os.system("rm -r {}".format(tmp_dir))
+		message.append("COMPLETED")
 	else:
 		create_sorted_fastq_file (read_two, group_barcode_matrix, coord_barcode_matrix, output_dir, indices_list)
 		message.append("COMPLETED")
