9fba6f0f3320da4d77a100bcce1f26dd818717f2
MichalSernero <z5134069@student.unsw.edu.au>
2020-08-03 14:52:43 +1000

    Added comments and removed Samtools path in bam to bigwig

--- bam_to_bigwig.sh
+++ bam_to_bigwig.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-# pass in the directory to the experiment/sample
+# UNSW BINF6111 Team Voineagu
+# Authors: Caitlin Ramsay, Michal Sernero, Sehhaj Grewal
+# Date: 2 August 2020
 
-# can use star aligner to output both BAM and wiggle
-# --outSAMtype BAM Unsorted SortedByCoordinate
-# --outWigType wiggle
+# Function: Converts BAM files into BigWig files using the bamCoverage function of deepTools
+# Version of deepTools used in this version: 3.3.0
 
+# For more information on bamCoverage, use the link below:
+# https://deeptools.readthedocs.io/en/develop/content/tools/bamCoverage.html
+
+
+#Directories of the Working Directory and the pathway to deepTools bamCoverage are given as arguments
 WORKING_DIR=$1
 EXPERIMENT_DIREC="${WORKING_DIR}/SORTED_GROUPS/"
 BAMCOVERAGE_RUN=$2
-SAMTOOLS_RUN=$3
 
 SUB_DIRECS=$(ls "$EXPERIMENT_DIREC")
+
+# Goes through each directory for each sample, and given a BAM and indexed BAM file
+# run bamCoverage on the BAM file in the directory
+# where -b, --bam is the input BAM file
+# -o, --outFileName is the filename given for the Output file
+# -of, --outFileFormat is the format of the Output file
+# currently conversion chosen is bigwig
+# can also be converted to bedgraph if chosen
+# --normalizeUsing allows for normalization on the reads to be performed
+# currently being normalised with CPM
+# other options available include: RPKM, BPM, RPGC, None
+# -p, --numberofProcessors allows for the use of multiple processors to be used
+# current amount being used is 8
 
 for direc in $SUB_DIRECS
 do
